fa549d7fa0c916ffb316635dd55191527044d3ce
Chris Mickley <chris.mickley@gmail.com>
2019-04-09 12:08:02 -0400

    Added submit form for code and created Controller with GET method

---
+++ final-capstone-project/src/main/java/com/techelevator/controller/CodeController.java
@@ -0,0 +1,16 @@
+package com.techelevator.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CodeController {
+
+	@RequestMapping("/submitCode")
+	public String displayCodeSubmissionForm(HttpServletRequest request) {
+		return "submitCode";
+		
+	}
+}
