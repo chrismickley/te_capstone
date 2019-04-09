fb441ac65f43ff02bffc0e25f36c7b8d9cc5c6cf
bruce stull <bruce.stull@gmail.com>
2019-04-09 19:00:21 -0400

    modified CodeSnippetDAO.java

--- final-capstone-project/src/main/java/com/techelevator/model/CodeSnippetDAO.java
+++ final-capstone-project/src/main/java/com/techelevator/model/CodeSnippetDAO.java
@@ -5,5 +5,6 @@ import java.util.List;
 public interface CodeSnippetDAO {
 	
 	public List<CodeSnippet> getAllCodeSnippets();
+	// getSnippetById();
 	public void addCodeSnippet(CodeSnippet codeSnippet);
 }
